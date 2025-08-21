480f1c78719e2a751f53bab924c0e99b89570e29
Utkarsh Kumar <utkarsh_k@cs.iitr.ac.in>
2025-08-21 12:03:16 +0530

    Delete task-5 directory

--- task-5/src/components/EventMarker.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventMarker: React.FC<{ year: string }> = ({ year }) => {
-  return <div className="event-marker">{year}</div>;
-};
-
-export default EventMarker;
--- task-5/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface EventItem {
-  year: string; 
-  title: string;
-  desc: string;
-  img: string;
-  category: string;
-}
